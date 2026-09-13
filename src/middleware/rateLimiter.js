b723f83bfa1c3e2a34b4594671a41f082775b26e
MasudRana924 <masud15-924@diu.edu.bd>
2026-09-13 23:43:12 +0600

    done

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 requests per windowMs
+  windowMs: 150 * 60 * 1000, // 15 minutes
+  max: 50, // 5 requests per windowMs
   message: {
     success: false,
     message: 'Too many authentication attempts, please try again later.',
